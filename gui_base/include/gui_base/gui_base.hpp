5b9aedb193d249a160d86648807a9de4976087de
SimonMaracine <simonmaracine@gmail.com>
2023-09-23 18:20:25 +0300

    Update ImGuiFileDialog

--- gui_base/include/gui_base/gui_base.hpp
+++ gui_base/include/gui_base/gui_base.hpp
@@ -38,5 +38,5 @@ namespace gui_base {
 
     inline constexpr unsigned int VERSION_MAJOR = 0;
     inline constexpr unsigned int VERSION_MINOR = 2;
-    inline constexpr unsigned int VERSION_PATCH = 0;
+    inline constexpr unsigned int VERSION_PATCH = 1;
 }
