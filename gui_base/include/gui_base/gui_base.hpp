13a5cb224d03af5d222338033e5ee27151f0bd8c
SimonMaracine <simonmaracine@gmail.com>
2024-08-22 13:25:23 +0300

    Upgrade Dear ImGui

--- gui_base/include/gui_base/gui_base.hpp
+++ gui_base/include/gui_base/gui_base.hpp
@@ -47,5 +47,5 @@ namespace gui_base {
 
     inline constexpr unsigned int VERSION_MAJOR {0};
     inline constexpr unsigned int VERSION_MINOR {8};
-    inline constexpr unsigned int VERSION_PATCH {3};
+    inline constexpr unsigned int VERSION_PATCH {4};
 }
